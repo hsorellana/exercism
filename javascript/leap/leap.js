fe890a1dd0d921829252d726e8f7970ff6a596a7
Hugo Soto <hsotoorellana@gmail.com>
2017-03-14 22:03:35 -0300

    Leap year exercise

---
+++ javascript/leap/leap.js
@@ -0,0 +1,8 @@
+var Year = function() {};
+
+Year.prototype.isLeap = function(input) {
+  input = parseInt(input);
+  return input % 4 === 0 && (input % 100 !== 0 || input % 400 === 0); 
+};
+
+module.exports = Year;
